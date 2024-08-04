4205f95c6de463a6021e527c687706f23c467280
Pawel Paluch <info@pixelvision.pl>
2024-08-04 14:52:15 +0200

    Added key in <li>

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Tasks = (props) => (
     <ul className="tasks">
         {props.tasks.map(task =>( 
-            <li
+            <li key={task.id}
             className={`tasks__item${task.done && props.hideDoneTasks ? " tasks__item--hidden" : ""}`}
             >
              <button className="tasks__button tasks__button--toggleDone">
