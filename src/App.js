9520cc67922153c146d85b2b018ea694a30dbb7b
Pawel Paluch <info@pixelvision.pl>
2024-08-25 20:56:38 +0200

    remove task interaction

--- src/App.js
+++ src/App.js
@@ -6,19 +6,20 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 
-const tasks = [
-    { id: 1, content: "przejść na reacta", done: false },
-    { id: 2, content: "wyjść na spacer z psem", done: true },
-];
-
-
 function App() {
-
     const [hideDone, setHideDone] = useState(false);
+    const [tasks, setTasks] = useState([
+        { id: 1, content: "przejść na reacta", done: false },
+        { id: 2, content: "wyjść na spacer z psem", done: true },
+    ]);
 
     const toggleHideDone = () => {
         setHideDone(hideDone => !hideDone);
 
+    };
+
+    const removeTask = (id) => {
+        setTasks(tasks => tasks.filter(task => task.id !== id));
     };
 
     return (
@@ -31,12 +32,14 @@ function App() {
 
             <Section
                 title="Lista zadań"
-                body={<Tasks tasks={tasks} hideDone={hideDone} />}
+                body={
+                    <Tasks tasks={tasks} hideDone={hideDone} removeTask={removeTask}/>
+                }
                 extraHeaderContent={
-                <Buttons 
-                tasks={tasks} 
-                hideDone={hideDone} 
-                toggleHideDone={toggleHideDone} />}
+                    <Buttons
+                        tasks={tasks}
+                        hideDone={hideDone}
+                        toggleHideDone={toggleHideDone} />}
             />
         </Container>
     );
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,10 +1,15 @@
 import "./style.css";
 
-const Tasks = (props) => (
+const Tasks = ({tasks, hideDone, removeTask}) => (
     <ul className="tasks">
-        {props.tasks.map(task =>( 
-            <li key={task.id}
-            className={`tasks__item${task.done && props.hideDone ? " tasks__item--hidden" : ""}`}
+        {tasks.map(task =>( 
+            <li 
+            key={task.id}
+            className={`tasks__item${
+                task.done && hideDone 
+                ? " tasks__item--hidden"
+                : ""}`
+            }
             >
              <button className="tasks__button tasks__button--toggleDone">
         {task.done ? "✔" : ""}
@@ -12,7 +17,9 @@ const Tasks = (props) => (
         <span className={`tasks__content${task.done ? " tasks__content--done" : ""}"`}>
             {task.content}
             </span>
-        <button className="tasks__button tasks__button--remove">
+        <button className="tasks__button tasks__button--remove"
+        onClick={() => removeTask(task.id)}
+        >
         🗑
         </button>
             </li>
