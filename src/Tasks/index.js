a5f6419d66adbcac6e6472aef65fea54c7c47db3
Pawel Paluch <info@pixelvision.pl>
2024-08-25 21:17:24 +0200

    toggle task done interaction

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,27 +1,29 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideDone, removeTask}) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
     <ul className="tasks">
-        {tasks.map(task =>( 
-            <li 
-            key={task.id}
-            className={`tasks__item${
-                task.done && hideDone 
-                ? " tasks__item--hidden"
-                : ""}`
-            }
+        {tasks.map(task => (
+            <li
+                key={task.id}
+                className={`tasks__item${task.done && hideDone
+                        ? " tasks__item--hidden"
+                        : ""}`
+                }
             >
-             <button className="tasks__button tasks__button--toggleDone">
-        {task.done ? "✔" : ""}
-        </button>
-        <span className={`tasks__content${task.done ? " tasks__content--done" : ""}"`}>
-            {task.content}
-            </span>
-        <button className="tasks__button tasks__button--remove"
-        onClick={() => removeTask(task.id)}
-        >
-        🗑
-        </button>
+                <button
+                    className="tasks__button tasks__button--toggleDone"
+                    onClick={() => toggleTaskDone(task.id)}
+                    >
+                    {task.done ? "✔" : ""}
+                </button>
+                <span className={`tasks__content${task.done ? " tasks__content--done" : ""}"`}>
+                    {task.content}
+                </span>
+                <button className="tasks__button tasks__button--remove"
+                    onClick={() => removeTask(task.id)}
+                >
+                    🗑
+                </button>
             </li>
         ))}
     </ul>
