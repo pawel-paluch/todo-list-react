8093b46e979bced0a7636b1f81566a1ff6912614
Pawel Paluch <info@pixelvision.pl>
2024-08-25 17:20:01 +0200

    toggle HideDone interaction

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -9,12 +10,20 @@ const tasks = [
     { id: 1, content: "przejść na reacta", done: false },
     { id: 2, content: "wyjść na spacer z psem", done: true },
 ];
-const hideDone = false;
+
 
 function App() {
+
+    const [hideDone, setHideDone] = useState(false);
+
+    const toggleHideDone = () => {
+        setHideDone(hideDone => !hideDone);
+
+    };
+
     return (
         <Container>
-    <Header title="Lista zadań" />
+            <Header title="Lista zadań" />
             <Section
                 title="Dodaj nowe zadanie"
                 body={<Form />}
@@ -23,7 +32,11 @@ function App() {
             <Section
                 title="Lista zadań"
                 body={<Tasks tasks={tasks} hideDone={hideDone} />}
-                extraHeaderContent={<Buttons tasks={tasks} hideDone={hideDone} />}
+                extraHeaderContent={
+                <Buttons 
+                tasks={tasks} 
+                hideDone={hideDone} 
+                toggleHideDone={toggleHideDone} />}
             />
         </Container>
     );
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = (props) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
 
     <div className="buttons">
-        {props.tasks.length > 0 && (
+        {tasks.length > 0 && (
             <>
-                <button className="buttons__button">
-                    {props.hideDone ? "Pokaż" : "Ukryj"} ukończone
+                <button onClick={toggleHideDone} className="buttons__button">
+                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
                 </button>
                 <button className="buttons__button"
-                    disabled={props.tasks.every(({ done }) => done)}>
+                    disabled={tasks.every(({ done }) => done)}>
                     Ukończ wszystkie
                 </button>
             </>
