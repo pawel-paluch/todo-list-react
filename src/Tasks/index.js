fcd59b839f14de1bff667f487b76c7e32dd23ad1
Pawel Paluch <info@pixelvision.pl>
2024-11-11 21:03:27 +0100

    Refactor Tasks component: replace CSS with styled-components for enhanced modularity and readability

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,34 +1,19 @@
-import "./style.css";
+import { List, Item, Content, Button } from "./styled";
 
 const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-    <ul className="tasks">
-        {tasks.map(task => (
-            <li
-                key={task.id}
-                className={`tasks__item${task.done && hideDone
-                        ? " tasks__item--hidden"
-                        : ""}`
-                }
-            >
-                <button
-                    className="tasks__button tasks__button--toggleDone"
-                    onClick={() => toggleTaskDone(task.id)}
-                >
+    <List>
+        {tasks.map((task) => (
+            <Item key={task.id} hidden={task.done && hideDone}>
+                <Button toggleDone onClick={() => toggleTaskDone(task.id)}>
                     {task.done ? "✔" : ""}
-                </button>
-                <span className={
-                    `tasks__content${task.done ? " tasks__content--done" : ""}`}
-                    >
-                    {task.content}
-                </span>
-                <button className="tasks__button tasks__button--remove"
-                    onClick={() => removeTask(task.id)}
-                >
+                </Button>
+                <Content done={task.done}>{task.content}</Content>
+                <Button remove onClick={() => removeTask(task.id)}>
                     🗑
-                </button>
-            </li>
+                </Button>
+            </Item>
         ))}
-    </ul>
+    </List>
 );
 
 export default Tasks;
