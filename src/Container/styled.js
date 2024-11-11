ef10a2c5be93986f19e51e3f0f4026c7db0e9597
Pawel Paluch <info@pixelvision.pl>
2024-11-11 20:53:21 +0100

    Add styled-components file to replace CSS styling

---
+++ src/Container/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    max-width: 980px;
+    margin: 0 auto;
+    padding: 20px;
+`;
---
+++ src/Section/styled.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+    margin: 10px auto;
+    box-shadow: 0 0 5px ${({ theme }) => theme.color.alto};
+    background: ${({ theme }) => theme.color.white};
+`;
+
+export const Header = styled.header`
+    border-bottom: 1px solid ${({ theme }) => theme.color.alto};
+    display: grid;
+    grid-template-columns: auto auto;
+    grid-gap: 20px;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        grid-template-columns: 1fr;
+    }
+`;
+
+export const Title = styled.h2`
+    font-size: 20px;
+    margin: 0;
+`;
+
+export const Body = styled.div`
+    font-size: 20px;
+    margin: 0;
+`;
