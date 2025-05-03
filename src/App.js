371cfc6cd579c8f058de0806fa1264c4a1748b1b
Pawel Paluch <info@pixelvision.pl>
2025-05-03 19:26:41 +0200

    add routing with navigation and route components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import Tasks from "./features/tasks/TasksPage";
+import Task from "./features/tasks/TaskPage";
+import Author from "./features/author/AuthorPage";
+import { toAuthor, toTask, toTasks } from "./routing";
+import Navigation from "./Navigation";
 
-export default () => <Tasks />
+
+export default () => (
+    <HashRouter>
+        <Navigation />
+
+        <Switch>
+            <Route path={toTask()}>
+                <Task />
+            </Route>
+            <Route path={toTasks()}>
+                <Tasks />
+            </Route>
+            <Route path={toAuthor()}>
+                <Author />
+            </Route>
+            <Route>
+                <Redirect to={toTasks()} />
+            </Route>
+        </Switch>
+    </HashRouter>
+);
