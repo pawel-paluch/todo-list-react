53de17faa09ee3ab8b94be56956a964dcd0b4da3
Pawel Paluch <info@pixelvision.pl>
2025-05-03 19:32:22 +0200

    extend taskSlice with loading state and query-based task selectors

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -27,9 +27,17 @@ const tasksSlice = createSlice({
                 task.done = true;
             }
         },
-        fetchExampleTasks: () => { },
-        setTasks: (state, { payload: tasks }) => {
+        fetchExampleTasks: state => {
+            state.loading = true;
+        },
+
+        fetchExampleTasksSuccess: (state, { payload: tasks }) => {
             state.tasks = tasks;
+            state.loading = false;
+        },
+
+        fetchExampleTasksError: (state) => {
+            state.loading = false;
         },
     },
 });
@@ -40,15 +48,31 @@ export const {
     toggleTaskDone,
     removeTask,
     setAllDone,
-    setTasks,
-    fetchExampleTasks
-} = tasksSlice.actions;
+    fetchExampleTasks,
+    fetchExampleTasksSuccess,
+    fetchExampleTasksError,
+  } = tasksSlice.actions;
 
 const selectTasksState = state => state.tasks;
 
 export const selectTasks = state => selectTasksState(state).tasks;
 export const selectHideDone = state => selectTasksState(state).hideDone
+export const selectLoading = state => selectTasksState(state).loading;
 export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
+
+export const selectTaskById = (state, taskId) =>
+    selectTasks(state).find(({ id }) => id === taskId);
+
+export const selectTasksByQuery = (state, query) => {
+    const tasks = selectTasks(state);
+
+    if (!query || query.trim() === "") {
+        return tasks;
+    }
+
+    return selectTasks(state).filter(({ content }) =>
+        content.toUpperCase().includes(query.trim().toUpperCase()));
+}
 
 export default tasksSlice.reducer;
