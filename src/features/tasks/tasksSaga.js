a3ef0d85eac5d7f1f84d720d56d8371083fb26f2
Pawel Paluch <info@pixelvision.pl>
2025-05-17 19:34:14 +0200

    add loading state handling to saga and extend fetch delay

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,25 +1,32 @@
 import { takeEvery, call, put, delay, select } from "redux-saga/effects";
-import { fetchExampleTasks, fetchExampleTasksSuccess, selectTasks, fetchExampleTasksError } from "./taskSlice";
+import {
+  fetchExampleTasks,
+  fetchExampleTasksSuccess,
+  fetchExampleTasksError,
+  startLoading,
+  selectTasks,
+} from "./taskSlice";
 import { getExampleTasks } from "./getExampleTasks";
 import { saveTasksInLocalStorage } from "./tasksLocalStorage";
 
 function* fetchExampleTasksHandler() {
-    try {
-        yield delay(1000);
-        const exampleTasks = yield call(getExampleTasks);
-        yield put(fetchExampleTasksSuccess(exampleTasks));
-    } catch (error) {
-        yield put(fetchExampleTasksError());
-        yield call(alert, "Uwaga, wystąpił błąd!");
-    }
+  try {
+    yield put(startLoading());
+    yield delay(2000);
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(fetchExampleTasksSuccess(exampleTasks));
+  } catch (error) {
+    yield put(fetchExampleTasksError());
+    yield call(alert, "Uwaga, wystąpił błąd!");
+  }
 }
 
 function* saveTasksInLocalStorageHandler() {
-    const tasks = yield select(selectTasks);
-    yield call(saveTasksInLocalStorage, tasks);
+  const tasks = yield select(selectTasks);
+  yield call(saveTasksInLocalStorage, tasks);
 }
 
 export function* tasksSaga() {
-    yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
-    yield takeEvery("*", saveTasksInLocalStorageHandler);
+  yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
+  yield takeEvery("*", saveTasksInLocalStorageHandler);
 }
