9f27a2da9ddfac52960e6ce2c7e45e9e57984b3e
Pawel Paluch <info@pixelvision.pl>
2025-05-17 19:28:22 +0200

    use transient props ($) in styled components to fix React warnings

--- src/features/tasks/TasksPage/TaskList/styled.js
+++ src/features/tasks/TasksPage/TaskList/styled.js
@@ -2,72 +2,74 @@ import styled, { css } from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const List = styled.ul`
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
 `;
 
 export const Item = styled.li`
-    border-bottom: 1px solid ${({ theme }) => theme.color.alto};
-    display: grid;
-    grid-template-columns: auto 1fr auto;
-    grid-gap: 10px;
-    align-items: center;
-    padding: 10px;
+  border-bottom: 1px solid ${({ theme }) => theme.color.alto};
+  display: grid;
+  grid-template-columns: auto 1fr auto;
+  grid-gap: 10px;
+  align-items: center;
+  padding: 10px;
 
-    ${({ hidden }) =>
+  ${({ hidden }) =>
         hidden &&
         css`
-            display: none;
-        `}
+      display: none;
+    `}
 `;
 
 export const Content = styled.span`
-    ${({ done }) =>
-        done &&
+  ${({ $done }) =>
+        $done &&
         css`
-            text-decoration: line-through;
-        `}
+      text-decoration: line-through;
+    `}
 `;
 
 export const Button = styled.button`
-    border: none;
-    color: ${({ theme }) => theme.color.white};
-    width: 30px;
-    height: 30px;
-    padding: 0;
-    transition: filter 0.3s;
+  border: none;
+  color: ${({ theme }) => theme.color.white};
+  width: 30px;
+  height: 30px;
+  padding: 0;
+  transition: filter 0.3s;
 
-    ${({ toggleDone }) =>
-        toggleDone &&
+  ${({ $toggleDone }) =>
+        $toggleDone &&
         css`
-            background: ${({ theme }) => theme.color.forestGreen};
-        `}
-    
-    ${({ remove }) =>
-        remove &&
+      background: ${({ theme }) => theme.color.forestGreen};
+    `}
+
+  ${({ $remove }) =>
+        $remove &&
         css`
-            background: ${({ theme }) => theme.color.crimson};
-        `}
+      background: ${({ theme }) => theme.color.crimson};
+    `}
 
-    &:hover {
-        filter: brightness(110%);
-    }
+  &:hover {
+    filter: brightness(110%);
+    cursor: pointer;
+  }
 
-    &:active {
-        filter: brightness(120%);
-    }
+  &:active {
+    filter: brightness(120%);
+  }
 `;
 
 export const StyledLink = styled(NavLink)`
-    text-decoration: none;
-    color: ${({ theme }) => theme.color.teal};
+  text-decoration: none;
+  color: ${({ theme }) => theme.color.teal};
 
-    &:hover {
-        text-decoration: underline;
-    }
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
 
-    &.active {
-        font-weight: bold;
-    }
+  &.active {
+    font-weight: bold;
+  }
 `;
